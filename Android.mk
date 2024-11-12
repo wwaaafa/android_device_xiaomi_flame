93e71d8687ab20d7d66719c92754b8fd5f62f4a0
wwaaafa <153055137+wwaaafa@users.noreply.github.com>
2024-11-12 20:14:16 +0800

    Add files via upload

--- Android.mk
+++ Android.mk
@@ -1,10 +1,12 @@
-# Android device tree for Redmi flame (flame)
-
-```
 #
 # Copyright (C) 2024 The Android Open Source Project
 # Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),flame)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,10 +1,14 @@
-# Android device tree for Redmi flame (flame)
-
-```
 #
 # Copyright (C) 2024 The Android Open Source Project
 # Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_flame.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_flame-user \
+    omni_flame-userdebug \
+    omni_flame-eng
